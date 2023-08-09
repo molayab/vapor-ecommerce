6a96ccdecc599079e7af24c666eff6190d5577de
Mateo Olaya <dev.mateo.olaya@gmail.com>
2023-08-09 13:12:09 -0500

    Added variants and reviews controller

--- Sources/App/Controllers/ProductReviewsController.swift
+++ Sources/App/Controllers/ProductReviewsController.swift
@@ -10,6 +10,89 @@ struct ProductReviewsController: RouteCollection {
         ])
         
         restricted.post(use: addReview)
+        restricted.get(use: listReviews)
+        restricted.delete(":reviewId", use: deleteReview)
+        restricted.patch(":reviewId", use: editReview)
+    }
+    
+    private func deleteReview(req: Request) async throws -> HTTPStatus {
+        guard let uuid = req.parameters.get("productId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let reviewId = req.parameters.get("reviewId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let product = try await Product.find(uuid, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        let user = try req.auth.require(User.self)
+        guard let review = try await product
+            .$reviews
+            .query(on: req.db)
+            .filter(\.$id == reviewId)
+            .first() else {
+            
+            throw Abort(.notFound)
+        }
+        
+        guard try review.$user.id == user.requireID() else {
+            throw Abort(.forbidden)
+        }
+        
+        try await review.delete(on: req.db)
+        return .ok
+    }
+    
+    private func editReview(req: Request) async throws -> ProductReview.Public {
+        guard let uuid = req.parameters.get("productId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let reviewId = req.parameters.get("reviewId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let product = try await Product.find(uuid, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        let user = try req.auth.require(User.self)
+        guard let review = try await product
+            .$reviews
+            .query(on: req.db)
+            .filter(\.$id == reviewId)
+            .first() else {
+            
+            throw Abort(.notFound)
+        }
+        
+        guard try review.$user.id == user.requireID() else {
+            throw Abort(.forbidden)
+        }
+        
+        let payload = try req.content.decode(ProductReview.Create.self)
+        try ProductReview.Create.validate(content: req)
+        
+        review.comment = payload.comment
+        review.score = payload.score
+        
+        try await review.update(on: req.db)
+        return try review.asPublic()
+    }
+    
+    private func listReviews(req: Request) async throws -> [ProductReview.Public] {
+        guard let uuid = req.parameters.get("productId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        guard let product = try await Product.find(uuid, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        return try await product.$reviews.get(on: req.db).map { try $0.asPublic() }
     }
     
     private func addReview(req: Request) async throws -> ProductReview.Public {
--- Sources/App/Controllers/ProductsVariantsController.swift
+++ Sources/App/Controllers/ProductsVariantsController.swift
@@ -1,11 +1,6 @@
 import Fluent
 import Vapor
 
-struct ProductVariantDeleteModel: Content {
-    let status: String
-    let wasProductDeleted: Bool
-}
-
 struct ProductsVariantsController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
         let products = routes.grouped("products")
@@ -17,6 +12,40 @@ struct ProductsVariantsController: RouteCollection {
         
         restricted.post(":productId", "variants", use: create)
         restricted.delete(":productId", "variants", ":variantId", use: delete)
+        restricted.patch(":productId", "variants", ":variantId", use: editVariant)
+    }
+    
+    private func editVariant(req: Request) async throws -> ProductVariant.Public {
+        guard let uuid = req.parameters.get("productId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        guard let variantId = req.parameters.get("variantId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        guard let product = try await Product.find(uuid, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        
+        let user = try req.auth.require(User.self)
+        let payload = try req.content.decode(ProductVariant.Create.self)
+        try ProductVariant.Create.validate(content: req)
+        
+        guard let variant = try await product
+            .$variants
+            .query(on: req.db)
+            .filter(\.$id == variantId).first() else {
+                throw Abort(.notFound)
+        }
+        
+        variant.name = payload.name
+        variant.price = payload.price
+        variant.salePrice = payload.salePrice
+        variant.sku = payload.sku
+        variant.stock = payload.stock
+        variant.isAvailable = payload.availability
+        
+        try await variant.save(on: req.db)
+        return try await variant.asPublic(on: req.db)
     }
     
     private func create(req: Request) async throws -> ProductVariant.Public {
@@ -34,35 +63,25 @@ struct ProductsVariantsController: RouteCollection {
         return try await variant.asPublic(on: req.db)
     }
     
-    private func delete(req: Request) async throws -> ProductVariantDeleteModel {
-        guard let uuid = req.parameters.get("variantId", as: UUID.self) else {
+    private func delete(req: Request) async throws -> Response {
+        guard let uuid = req.parameters.get("productId", as: UUID.self) else {
             throw Abort(.badRequest)
         }
-        guard let variant = try await ProductVariant.find(uuid, on: req.db) else {
+        guard let variantId = req.parameters.get("variantId", as: UUID.self) else {
+            throw Abort(.badRequest)
+        }
+        guard let product = try await Product.find(uuid, on: req.db) else {
             throw Abort(.notFound)
         }
         
-        // Products MUST NOT have empty variants
-        // If it is the last variant, delete the product
-        let product = try await variant.$product.get(on: req.db)
-        let variants = try await product.$variants.get(on: req.db)
-        for image in try await variant.$images.get(on: req.db) {
-            try await image.delete(on: req.db)
+        guard let variant = try await product
+            .$variants
+            .query(on: req.db)
+            .filter(\.$id == variantId).first() else {
+                throw Abort(.notFound)
         }
         
-        if variants.count <= 1 {
-            try await variant.delete(on: req.db)
-            try await variant.product.delete(on: req.db)
-            return ProductVariantDeleteModel(
-                status: "deleted",
-                wasProductDeleted: true
-            )
-        } else {
-            try await variant.delete(on: req.db)
-            return ProductVariantDeleteModel(
-                status: "deleted",
-                wasProductDeleted: false
-            )
-        }
+        try await variant.delete(on: req.db)
+        return Response(status: .ok)
     }
 }
