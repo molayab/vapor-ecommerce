52c2b2f1bee99e20015e7e1f713c0dac6f76a9d0
Mateo Olaya <dev.mateo.olaya@gmail.com>
2023-08-29 09:07:23 -0500

    Added Linux full support

--- Sources/App/Controllers/Misc/SettingsController.swift
+++ Sources/App/Controllers/Misc/SettingsController.swift
@@ -40,14 +40,6 @@ struct SettingsController: RouteCollection {
     }
 
     private func getSettings(req: Request) async throws -> Settings.Public {
-        // get the settings from .json file
-        let settings = req.application.directory.workingDirectory
-        return try ContentConfiguration()
-            .requireDecoder(for: .json)
-            .decode(Settings.self,
-                    from: try await req.fileio.collectFile(
-                        at: settings + "/settings.json"),
-                    headers: .init()).asPublic()
+        return settings.asPublic()
     }
-
 }
